3cc87cc1766f605f5f7f15df017b944b35c2e52d
tangmingheng <mhtang@thoughtworks.com>
2020-06-03 13:45:24 +0800

    transform数据

--- spark_job.py
+++ spark_job.py
@@ -1,7 +1,10 @@
+import datetime
 import sys
 from argparse import ArgumentParser
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col, dayofmonth, concat, lit
+from pyspark.sql.types import FloatType, StringType, DateType
 
 
 class GdeltJob:
@@ -16,7 +19,8 @@ class GdeltJob:
 
         return data_frame.toDF("GLOBALEVENTID", "SQLDATE", "MonthYear", "Year", "FractionDate", "Actor1Code",
                                "Actor1Name",
-                               "Actor1CountryCode", "Actor1KnownGroupCode", "Actor1EthnicCode", "Actor1Religion1Code",
+                               "Actor1CountryCode", "Actor1KnownGroupCode", "Actor1EthnicCode",
+                               "Actor1Religion1Code",
                                "Actor1Religion2Code", "Actor1Type1Code", "Actor1Type2Code", "Actor1Type3Code",
                                "Actor2Code",
                                "Actor2Name", "Actor2CountryCode", "Actor2KnownGroupCode", "Actor2EthnicCode",
@@ -28,13 +32,35 @@ class GdeltJob:
                                "NumSources", "NumArticles", "AvgTone", "Actor1Geo_Type", "Actor1Geo_FullName",
                                "Actor1Geo_CountryCode", "Actor1Geo_ADM1Code", "Actor1Geo_ADM2Code", "Actor1Geo_Lat",
                                "Actor1Geo_Long",
-                               "Actor1Geo_FeatureID", "Actor2Geo_Type", "Actor2Geo_FullName", "Actor2Geo_CountryCode",
+                               "Actor1Geo_FeatureID", "Actor2Geo_Type", "Actor2Geo_FullName",
+                               "Actor2Geo_CountryCode",
                                "Actor2Geo_ADM1Code", "Actor2Geo_ADM2Code", "Actor2Geo_Lat", "Actor2Geo_Long",
                                "Actor2Geo_FeatureID",
-                               "ActionGeo_Type", "ActionGeo_FullName", "ActionGeo_CountryCode", "ActionGeo_ADM1Code",
+                               "ActionGeo_Type", "ActionGeo_FullName", "ActionGeo_CountryCode",
+                               "ActionGeo_ADM1Code",
                                "ActionGeo_ADM2Code", "ActionGeo_Lat", "ActionGeo_Long", "ActionGeo_FeatureID",
                                "DATEADDED",
                                "SOURCEURL")
+
+    def transform(self, df):
+        date_convert_func = udf(lambda x: datetime.datetime.strptime(str(x), '%Y%m%d'), DateType())
+        iso_format_func = udf(lambda x: x.isoformat(), StringType())
+        return df \
+            .withColumn('eventId', col('GLOBALEVENTID')) \
+            .withColumn('eventDay', date_convert_func(col('SQLDATE'))) \
+            .withColumn('sourceUrl', col('SOURCEURL')) \
+            .withColumn('latitude', col('Actor1Geo_Lat').cast(FloatType())) \
+            .withColumn('longitude', col('Actor2Geo_Long').cast(FloatType())) \
+            .dropDuplicates(['sourceUrl']) \
+            .withColumn('timestamp', iso_format_func(col('eventDay'))) \
+            .withColumn('day', dayofmonth(col('eventDay'))) \
+            .withColumn('location', concat(col("latitude"), lit(","), col("longitude"))) \
+            .select('eventId', 'timestamp', 'day', 'sourceUrl', 'location', 'latitude', 'longitude') \
+            .cache()
+
+    def run(self, spark):
+        df = self.operate_data(spark)
+        return self.transform(df)
 
 
 def load_args(argv):
@@ -56,9 +82,9 @@ def write_to_es(df, **kwargs):
         .option('es.nodes.wan.only', 'true') \
         .option("es.net.http.auth.user", kwargs.get("esUserName")) \
         .option("es.net.http.auth.pass", kwargs.get("esUserPassword")) \
-        .option('es.mapping.id', 'GLOBALEVENTID') \
+        .option('es.mapping.id', 'eventId') \
         .mode('append') \
-        .save("gdelt/gdelt")
+        .save("gdelt/_doc")
 
 
 def main(argv):
@@ -69,7 +95,7 @@ def main(argv):
         .config('spark.jars.packages', 'org.elasticsearch:elasticsearch-spark-20_2.11:7.4.2') \
         .getOrCreate()
 
-    t_df = spark_job.operate_data(spark_session)
+    t_df = spark_job.run(spark_session)
 
     t_df.cache()
     t_df.count()
